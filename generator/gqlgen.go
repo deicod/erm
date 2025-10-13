4b5ebab0e9cc2d0bb4bb86c22e6e00d1a55d70c7
Darko Luketic <info@icod.de>
2025-10-13 11:24:30 +0200

    fix: tidy gql dependencies on demand

--- generator/gqlgen.go
+++ generator/gqlgen.go
@@ -5,17 +5,22 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/99designs/gqlgen/api"
 	"github.com/99designs/gqlgen/codegen/config"
 )
 
 func runGQLGen(root string) error {
-	modulePath, err := detectModulePath(root)
-	if err != nil {
+	if err := ensureGQLDependencies(root); err != nil {
 		return err
 	}
-	if err := ensureGQLDependencies(root); err != nil {
+	return runGQLGenInternal(root)
+}
+
+func runGQLGenInternal(root string) error {
+	modulePath, err := detectModulePath(root)
+	if err != nil {
 		return err
 	}
 	cfgPath, err := ensureGQLGenConfig(root, modulePath)
@@ -41,13 +46,50 @@ func ensureGQLDependencies(root string) error {
 		"github.com/vektah/gqlparser/v2",
 	}
 	env := append(os.Environ(), "GO111MODULE=on")
+	tidyAttempted := false
 	for _, dep := range deps {
-		cmd := exec.Command("go", "mod", "download", dep)
-		cmd.Dir = root
-		cmd.Env = env
-		if output, err := cmd.CombinedOutput(); err != nil {
-			return fmt.Errorf("go mod download %s: %w\n%s", dep, err, string(output))
+		if err := downloadGQLDependency(root, env, dep, &tidyAttempted); err != nil {
+			return err
 		}
 	}
 	return nil
+}
+
+func downloadGQLDependency(root string, env []string, dep string, tidyAttempted *bool) error {
+	output, err := runGoModDownload(root, env, dep)
+	if err == nil {
+		return nil
+	}
+
+	if !*tidyAttempted && strings.Contains(output, "not a known dependency") {
+		tidyOut, tidyErr := runGoModTidy(root, env)
+		*tidyAttempted = true
+		if tidyErr != nil {
+			return fmt.Errorf("go mod download %s: dependency missing and go mod tidy failed: %w\n%s", dep, tidyErr, tidyOut)
+		}
+
+		output, err = runGoModDownload(root, env, dep)
+		if err == nil {
+			return nil
+		}
+		return fmt.Errorf("go mod download %s failed after go mod tidy: %w\n%s", dep, err, output)
+	}
+
+	return fmt.Errorf("go mod download %s: %w\n%s", dep, err, output)
+}
+
+func runGoModDownload(root string, env []string, dep string) (string, error) {
+	cmd := exec.Command("go", "mod", "download", dep)
+	cmd.Dir = root
+	cmd.Env = env
+	output, err := cmd.CombinedOutput()
+	return string(output), err
+}
+
+func runGoModTidy(root string, env []string) (string, error) {
+	cmd := exec.Command("go", "mod", "tidy")
+	cmd.Dir = root
+	cmd.Env = env
+	output, err := cmd.CombinedOutput()
+	return string(output), err
 }
